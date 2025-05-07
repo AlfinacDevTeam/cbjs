15b45b0a3455288b705e29cd0ce614045ec5fb69
hoanghuy7 <hoanghuy200701@gmail.com>
2025-05-07 14:42:24 +0700

    commit code

--- chatbot.js
+++ chatbot.js
@@ -256,7 +256,7 @@ class ChatBot extends HTMLElement {
                 </div>
                 </div>
                 <div class="chat-input">
-                    <input id="messageInput" maxlength="200" placeholder="Nhập tin nhắn (200 tù)..." />
+                    <input id="messageInput" maxlength="200" placeholder="Nhập tin nhắn (200 từ)..." />
                     <button id="sendBtn">Gửi</button>
                 </div>
             </div>
